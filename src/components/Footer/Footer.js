ef019ce1ca118bd467a5e1ae4a36bf3e030ebfad
Siddharth Patel <sidharthpatel012@gmail.com>
2023-05-31 22:54:46 -0400

    Created footer as well. Simple nextjs portfolio development is complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,54 @@
-import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
 
-import { SocialIcons } from '../Header/HeaderStyles';
-import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+    CompanyContainer,
+    FooterWrapper,
+    LinkColumn,
+    LinkItem,
+    LinkList,
+    LinkTitle,
+    Slogan,
+    SocialContainer,
+    SocialIconsContainer,
+} from "./FooterStyles";
 
 const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  );
+    return (
+        <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle>Call</LinkTitle>
+                    <LinkItem href="tel:111-111-1111">111-111-1111</LinkItem>
+                </LinkColumn>
+
+                <LinkColumn>
+                    <LinkTitle>Email</LinkTitle>
+                    <LinkItem href="mailto:sidharthpatel012@gmail.com">
+                        sidharthpatel012@gmail.com
+                    </LinkItem>
+                </LinkColumn>
+            </LinkList>
+
+            <SocialIconsContainer>
+                <CompanyContainer>
+                    <Slogan>Inovating one project at a time</Slogan>
+                </CompanyContainer>
+                <SocialContainer>
+                    <SocialIcons href="https://github.com/sidharthpatel">
+                        <AiFillGithub size="3rem" />
+                    </SocialIcons>
+                    <SocialIcons href="https://linkedin.com/in/sidharthpatel01/">
+                        <AiFillLinkedin size="3rem" />
+                    </SocialIcons>
+                    <SocialIcons href="https://instagram.com/pzed01">
+                        <AiFillInstagram size="3rem" />
+                    </SocialIcons>
+                </SocialContainer>
+            </SocialIconsContainer>
+        </FooterWrapper>
+    );
 };
 
 export default Footer;
